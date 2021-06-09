08db2e7d001cad8f32cff9e8c623cdd619619e2e
sebby09559 <42832765+sebby09559@users.noreply.github.com>
2021-06-09 13:57:58 +0800

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -70,7 +70,7 @@ class DiscordClient
 			details: details,
 			state: state,
 			largeImageKey: 'icon',
-			largeImageText: "Friday Night Funkin: vs. bob",
+			largeImageText: "vs_bob",
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
